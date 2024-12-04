c2e737745cf3bae66f85be7fd9851c4179bafb3d
ruri <sub08041@naver.com>
2024-12-05 01:45:32 +0900

    feat 구매한 상품 페이지 완성

--- src/Components/BuyedItem.js
+++ src/Components/BuyedItem.js
@@ -1,28 +1,168 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import bread_img from '../Images/bread_img.png';
 import choc_img from '../Images/chocosora.jpg';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import butterroll from '../Images/butterroll.jfif';
+import reviewIcon from '../Images/reviewIcon.png';
 
 export default function Item() {
     const [quantity, setQuantity] = useState(null);
     const [priceUnit, setPriceUnit] = useState(null);
     const totalPrice = quantity ? Number(quantity) * priceUnit : 0;
-    const [buyedItem, setBuyedItem] = useState([]);
+    const [buyedItems, setBuyedItems] = useState([]);
+    const [isMobile, setIsMobile] = useState(false);
+    // 모바일 뷰, 태블릿 뷰 식별
+    const resizeHandler = () => {
+        setIsMobile(window.innerWidth <= 1024);
+    };
+    // 뷰포트 확인 후 조절
+    useEffect(() => {
+        resizeHandler(); // 초기 로드 시 크기 확인
+        window.addEventListener('resize', resizeHandler);
+        return () => {
+            window.removeEventListener('resize', resizeHandler);
+        };
+    }, []);
 
-    return <Container>구매한 제품 페이지</Container>;
+    const accessToken = Cookies.get('accessToken');
+
+    const fetchBuyedItem = async () => {
+        try {
+            const response = await axios.get(`https://breadend.shop/Mypage/histories`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            console.log('구매내역 불러오기 성공', response.data);
+            setBuyedItems(response.data);
+        } catch (error) {
+            console.error('API 요청에러', error);
+        }
+    };
+    useEffect(() => {
+        if (accessToken) {
+            fetchBuyedItem();
+        }
+    }, [accessToken]);
+
+    const PCView = () => {
+        return (
+            <Container>
+                <TitleContainer>구매내역</TitleContainer>
+                {buyedItems.map((product) => (
+                    <ProductContainer>
+                        <ProductTitleContainer>
+                            <div>상품 이미지</div>
+                            <div>상품 정보</div>
+                            <div>상품 가격</div>
+                            <div></div>
+                        </ProductTitleContainer>
+                        <ProductInfoContainer>
+                            <ReservImage src={butterroll}></ReservImage>
+                            <NameCostContainer>
+                                <Name>{product.itemname}</Name>
+                                <Info>{product.info}</Info>
+                            </NameCostContainer>
+                            <Cost>{product.price}</Cost>
+                            <Review>리뷰 작성하러 가기</Review>
+                        </ProductInfoContainer>
+                    </ProductContainer>
+                ))}
+            </Container>
+        );
+    };
+    const MobileView = () => {
+        return (
+            <Container>
+                <TitleContainer>구매내역</TitleContainer>
+                {buyedItems.map((product) => (
+                    <ProductContainer>
+                        <ProductTitleContainer>
+                            <div>상품 이미지</div>
+                            <div>상품 정보</div>
+                            <div>상품 가격</div>
+                        </ProductTitleContainer>
+                        <ProductInfoContainer>
+                            <ReservImage src={butterroll}></ReservImage>
+                            <NameCostContainer>
+                                <Name>{product.itemname}</Name>
+                                <Info>{product.info}</Info>
+                            </NameCostContainer>
+                            <Cost>{product.price}</Cost>
+                            <ReviewButton />
+                        </ProductInfoContainer>
+                    </ProductContainer>
+                ))}
+            </Container>
+        );
+    };
+
+    return <div style={{ width: '100%' }}>{isMobile ? <MobileView /> : <PCView />}</div>;
 }
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
+    width: 100%;
+    @media (max-width: 800px) {
+        padding: 1%;
+    }
+    box-sizing: border-box;
 `;
 
+const TitleContainer = styled.div`
+    font-size: 22px;
+    font-weight: bold;
+    border-bottom: 1px solid black;
+    padding: 10px 5px;
+    width: 100%;
+    @media (max-width: 800px) {
+        width: 95%;
+    }
+`;
+const ProductContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 10px 5px;
+`;
+const ProductTitleContainer = styled.div`
+    display: grid;
+    grid-template-columns: 150px 1.5fr 1fr;
+    margin-bottom: 1.5%;
+    @media (min-width: 1024px) {
+        grid-template-columns: 150px 1.5fr 1fr 1fr;
+    }
+    @media (max-width: 600px) {
+        grid-template-columns: 120px 1.5fr 1fr 1fr;
+    }
+    @media (max-width: 480px) {
+        grid-template-columns: 100px 1.5fr 1fr 1fr;
+    }
+`;
+const ProductInfoContainer = styled.div`
+    display: grid;
+    grid-template-columns: 150px 1.5fr 1fr;
+    position: relative;
+    @media (min-width: 1024px) {
+        grid-template-columns: 150px 1.5fr 1fr 1fr;
+    }
+    @media (max-width: 600px) {
+        grid-template-columns: 120px 1.5fr 1fr 1fr;
+    }
+    @media (max-width: 480px) {
+        grid-template-columns: 100px 1.5fr 1fr 1fr;
+    }
+`;
 const ReservImage = styled.div`
-    width: 17vw;
-    height: 17vw;
-    max-width: 120px;
-    max-height: 120px;
-    background-image: url(${(props) => props.backgroundImage});
+    width: 80px;
+    height: 80px;
+    max-width: 100px;
+    max-height: 100px;
+    background-image: url(${(props) => props.src});
     background-size: cover;
     background-position: center;
     text-align: center;
@@ -32,8 +172,47 @@ const ReservImage = styled.div`
     }
 
     @media (max-width: 480px) {
-        width: 80vw;
-        height: 80vw;
-        margin: 0 auto;
+        width: 80px;
+        height: 80px;
     }
+    border-radius: 50%;
+`;
+const NameCostContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    padding: 1% 0;
+`;
+const Name = styled.div`
+    margin-bottom: 5px;
+`;
+const Info = styled.div`
+    margin-bottom: 5px;
+    color: gray;
+`;
+const Cost = styled.div`
+    padding: 1% 0;
+`;
+const Review = styled.button`
+    cursor: pointer;
+    background-color: #d4b896;
+    border-radius: 20px;
+    padding: 10px;
+    width: 160px;
+    height: 40px;
+    align-items: center;
+    &:hover {
+        background-color: #bea587;
+    }
+`;
+const ReviewButton = styled.div`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    background-image: url(${reviewIcon});
+    width: 20px;
+    height: 20px;
+    background-size: contain;
+    cursor: pointer; // Optional: adds pointer cursor on hover
+    margin-right: 10px;
 `;
---
+++ src/Components/BuyerReview.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+export default function BuyerReview() {
+    return <div>리뷰작성</div>;
+}
